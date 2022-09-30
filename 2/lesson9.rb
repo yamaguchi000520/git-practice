806759388498ba58301136b0accccb4c674a8a9d
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-09-30 07:30:01 +0000

    [Finish] 2-9

--- 2/lesson9.rb
+++ 2/lesson9.rb
@@ -1,18 +1,10 @@
 class Car
   
-  def move(direction,distance)
-    self.turn(direction)
-    self.run(distance)
-  end
-  
-  def turn(direction)
+  def self.turn(direction)
     puts "#{direction}に曲がります"
   end
-  def run(distance)
-    puts "車で#{distance}キロ走ります"
-  end 
+  
 end
 
-car=Car.new
-car.move("右",5)
+Car.turn("右")
 
